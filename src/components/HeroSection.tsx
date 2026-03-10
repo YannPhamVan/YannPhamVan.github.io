2142053b93515bc3d3a3409ef4aef936b11caf31
Yann Pham-Van <2cl191+github@gadz.org>
2026-03-10 14:30:13 +0100

    Change period to comma in hero subtitle

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -57,8 +57,8 @@ export default function HeroSection() {
           Data Scientist freelance spécialisé en maintenance prédictive et scoring des risques via la{" "}
           <span style={{ color: "hsl(var(--performance-green-on-dark))" }}>
             détection d’anomalies
-          </span>.{" "}
-          J’aide les entreprises à exploiter leurs données pour{" "}
+          </span>,{" "}
+          j’aide les entreprises à exploiter leurs données pour{" "}
           <span style={{ color: "hsl(var(--performance-green-on-dark))" }}>
             améliorer leurs décisions
           </span>{" "}
